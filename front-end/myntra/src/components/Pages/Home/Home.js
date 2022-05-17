b6e409c1ffa6585f5913fb87b9e86681aff49e91
vmuralimohan <vignesh.muralimohan@perficient.com>
2022-05-18 00:24:45 +0530

    Deas of the day component created

--- front-end/myntra/src/components/Pages/Home/Home.js
+++ front-end/myntra/src/components/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Slider from '../../Custom Tools/Carousels/Slider'
+import DealsOfTheDays from './DealsOfTheDay/DealsOfTheDays'
 
 function Home() {
   return (
     <div>
     <Slider/>
+    <DealsOfTheDays/>
     </div>
   )
 }
